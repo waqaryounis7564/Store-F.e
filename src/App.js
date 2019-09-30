894084e9be5d84a1761dc9e89bd56faaba8753ef
waqar younis <waqaryounis7564@gmail.com>
2019-09-30 15:46:20 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>waqar</h1>
+      <i class="fas fa-camera"></i>
+    </div>
+  );
+}
+
+export default App;
